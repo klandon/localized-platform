461fe81827c304b1b9827ed4eb02150032ea3619
Kristopher Landon <landonkt@MBP16inch2021.localdomain>
2021-10-27 17:21:54 -0400

    small changes removing the kind

--- resource/local-k8s-platform/main.tf
+++ resource/local-k8s-platform/main.tf
@@ -2,17 +2,13 @@ locals {
   
 }
 
-module "local-k8s" {
-  source = "git::https://github.com/klandon/terraform-modules.git//kind-k8s"
-  disable_cni = false
-  deployment_group_label = "worker-landon"
-}
-
 module "argocd" {
   source = "git::https://github.com/klandon/terraform-modules.git//argocd"
   config_path = "~/.kube/config"
-  config_context = "kind-base"
-
+  config_context = "kind-kind"
 }
 
-
+#apps of apps
+# need to add metrics
+# observability
+# proxy config
